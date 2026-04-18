ee0cd3a93dc2befee3f00ae1b6411a8163061750
kucerja3 <kucerja3@ssakhk.cz>
2026-04-18 19:47:41 +0200

    Initial commit

--- src/main/java/pro1/apiDataModel/Action.java
+++ src/main/java/pro1/apiDataModel/Action.java
@@ -2,7 +2,8 @@ package pro1.apiDataModel;
 
 import com.google.gson.annotations.SerializedName;
 
-public class Action
-{
-
+public class Action{
+    public long obsazeni;
+    public Long ucitIdno;
+    public String den;
 }
